81a19423401ac20010a45f1bbff55061bc95e646
kingsalah94 <mohahamedbahachimis@gmail.com>
2023-12-28 00:03:35 +0100

    Gestion de stock ArticlesServices Implementation

---
+++ src/main/java/com/salah/gestiondestock/Services/ArticlesServiceImpl.java
@@ -0,0 +1,33 @@
+package com.salah.gestiondestock.Services;
+
+import com.salah.gestiondestock.Dtos.ArticlesDto;
+import com.salah.gestiondestock.Exceptions.ArticlesNotFoundException;
+
+import java.util.List;
+
+public class ArticlesServiceImpl implements ArticlesService{
+    @Override
+    public ArticlesDto addArticles(ArticlesDto articlesDto) {
+        return null;
+    }
+
+    @Override
+    public ArticlesDto updateArticles(ArticlesDto articlesDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteArticles(Integer id) {
+
+    }
+
+    @Override
+    public ArticlesDto getOneArticles(Integer id) throws ArticlesNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<ArticlesService> getAllArticles() {
+        return null;
+    }
+}
